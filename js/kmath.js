026eb6e73b686b37601a8336b9bf379a6acd9bf5
David Kirby <david@ecomatics.net>
2020-11-17 10:56:38 -0500

    Adds high precision digamma function (digamma12)

--- js/kmath.js
+++ js/kmath.js
@@ -21,6 +21,8 @@ export function kMath() {
     this.H2 = 0.57735026918962576450914878050196; // @constant {Number} 1/(sqrt(3))
     this.TWOLN2 = 1.3862943611198906188344642429164; /* @constant {Number} 2*ln(2) */
 
+    /* abs(ζ(1 - floor(2 k))) */
+    this.B2 = [1/12, 1/120, 1/252, 1/240, 1/132, 691/32760, 1/12, 3617/8160, 43867/14364, 174611/6600, 77683/276, 236364091/65520, 657931/12];
 
     /*
      * csc
@@ -368,7 +370,69 @@ export function kMath() {
      }
      return c += Math.log(b) - 0.5 / (b *= b) - (0.08333333333333333 - (0.008333333333333333 - (0.0039682539682539 - (0.004166666666666 - 1 / (132 * b)) / b) / b) / b) / b;
    };
+   
 
+   this.digamma12 = (x) => {
+    var PRECISION = 12;
+    var v = 0;
+    
+    /* If the absolute value of x is less than epsilon we assume zero */
+    /* TODO: this should return Complex Infinity */   
+    if (Math.abs(x) < Number.EPSILON) {      
+      return Infinity;
+    }
+    /* For negative integers we return Infinity */
+    /* TODO: this should return Complex Infinity */
+    if (Number.isInteger(x) && x < 0) {
+      return Infinity;
+    }
+    /* Special values (1) */
+    if (x === 1) {
+      return -K.GAMMA;
+    }
+    /* Special values (1/2) */
+    if (x === 1/2) {
+      return -K.GAMMA - K.TWOLN2;
+    }
+    /* Small values (0.000001) */
+    if (Math.abs(x) <= 1e-6) {
+      /* Positive x */
+      if (x > 0) {
+          return K.GAMMA - 1 / x + K.ZETA2;
+      }
+      /* Negative x */
+      if (x < 0) {
+          return digamma(1 - x) + K.PI / Math.tan(-K.PI * x);
+      }
+    }
+  
+    for (; PRECISION > x; x += 1) {
+      v -= 1.0 / x;
+    }
+  
+    return (v +=
+      Math.log(x) -
+      0.5 / (x *= x) -
+      (B2[0] -
+        (B2[1] -
+          (B2[2] -
+            (B2[3] -
+              (B2[4] -
+                (B2[5] -
+                  (B2[6] -
+                    (B2[7] -
+                      (B2[8] -
+                        (B2[9] - (B2[10] - (B2[11] - B2[12] / x) / x) / x) / x) /
+                        x) /
+                      x) /
+                    x) /
+                  x) /
+                x) /
+              x) /
+            x) /
+          x) /
+        x);
+    }
 
     /*
      * Harmonic Number
